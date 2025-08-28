bb14c41f5ecdbd4c62feddb6ec78854ef4c50d7f
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-08-29 00:39:23 +0800

    Add play round logic

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Input choice of either rock, paper, or scissors:");
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Draw! You both chose ${humanChoice}.`)
+    } else if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+    } else {
+        console.log(`You lose! ${humanChoice} is beat by ${computerChoice}.`)
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice().toLowerCase(), getComputerChoice())
