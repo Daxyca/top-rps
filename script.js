e32c33577296a4a312300e820bc8a4c7b430d3e9
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-08-29 00:30:01 +0800

    Add computer choice generator

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Script works!")
+function getComputerChoice() {
+    const rand = Math.floor(Math.random() * 3)
+    return rand === 0 ? "rock" : rand === 1 ? "paper" : "scissors";
+}
+
+console.log(getComputerChoice())
