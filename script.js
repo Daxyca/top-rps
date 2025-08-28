342a7ee222c159db9aec7cdec56114d5dca23124
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-08-29 00:49:10 +0800

    Add play game logic to play 5 rounds

--- script.js
+++ script.js
@@ -7,21 +7,38 @@ function getHumanChoice() {
     return prompt("Input choice of either rock, paper, or scissors:");
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log(`Draw! You both chose ${humanChoice}.`)
-    } else if (
-        humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "scissors" && computerChoice === "paper"
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-    } else {
-        console.log(`You lose! ${humanChoice} is beat by ${computerChoice}.`)
+function playGame() {
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`Draw! You both chose ${humanChoice}.`)
+        } else if (
+            humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper"
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+            humanScore++;
+        } else {
+            console.log(`You lose! ${humanChoice} is beat by ${computerChoice}.`)
+            computerScore++;
+        }
     }
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 0;
+    humanScore = 0;
+    computerScore = 0;
+    while (round < 5) {
+        round++
+        const humanSelection = getHumanChoice().toLowerCase();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection)
+    }
+    console.log(`Final scores:`)
+    console.log(`Human: ${humanScore}`)
+    console.log(`Computer: ${computerScore}`)
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-playRound(getHumanChoice().toLowerCase(), getComputerChoice())
+playGame()
